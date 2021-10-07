6029236a948be1bb749e8130164e93ed58961a11
Alper Tayfun <tayfun.alper@gmail.com>
2021-10-07 18:10:51 +0300

    te

---
+++ src/views/Home/components/TotalValueLockedCard.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Card, CardBody, Flex, Heading, Skeleton, Text } from '@orionswap/uikit'
+import { useTranslation } from 'contexts/Localization'
+import { useGetStats } from 'hooks/api'
+
+const StyledTotalValueLockedCard = styled(Card)`
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const Row = styled.div`
+  align-items: center;
+  display: flex;
+  font-size: 14px;
+  justify-content: space-between;
+  margin-bottom: 8px;
+`
+
+const TotalValueLockedCard = () => {
+  const { t } = useTranslation()
+  const data = useGetStats()
+  const totalLiquidity = 1700
+  const dailyVolume = 250
+
+  return (
+    <StyledTotalValueLockedCard>
+      <CardBody>
+        <Heading scale="xl" mb="24px">
+          {t('AMM Stats')}
+        </Heading>
+        {data ? (
+          <>
+            <Row>
+              <Text>{t('Total Liquidity')}:</Text>
+              <Text bold>${totalLiquidity}</Text>
+            </Row>
+            <Row>
+              <Text>{t('24H Volume')}:</Text>
+              <Text bold>${dailyVolume}</Text>
+            </Row>
+          </>
+        ) : (
+          <>
+          <Row>
+            <Text>{t('Total Liquidity')}:</Text>
+            <Text bold>${totalLiquidity}</Text>
+          </Row>
+          <Row>
+            <Text>{t('24H Volume')}:</Text>
+            <Text bold>${dailyVolume}</Text>
+          </Row>
+        </>
+        )}
+      </CardBody>
+    </StyledTotalValueLockedCard>
+  )
+}
+
+export default TotalValueLockedCard
